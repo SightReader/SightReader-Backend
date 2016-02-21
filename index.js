df7392e6e6f7fcb5fbb7ad8032623337ce43ba3a
Alain Galvan <hi@alain.xyz>
2016-02-21 13:17:28 -0500

    Nerge Front

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var express = require('express'), 
-    socketIo = require('socket.io'), 
+var express = require('express'),
+    socketIo = require('socket.io'),
     http = require('http'),
 
     compare2NotesLists = require('./compare2NotesLists'),
-    randomStringGenerator = require('./randomStringGenerator'), 
+    randomStringGenerator = require('./randomStringGenerator'),
     logger = require('./logger'),
     generateNotesStream = require('./generateNotesStream');
 
-var expressApp = express(), 
-    expressServer = expressApp.listen(8001),
+var expressApp = express(),
+    expressServer = expressApp.listen(8002),
     ioApp = socketIo(expressServer);
 
 expressApp.use(express.static('web'));
@@ -17,14 +17,14 @@ expressApp.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Methods',
                   'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers',
-                  'X-Requested-With, content-type, authorization, ' + 
+                  'X-Requested-With, content-type, authorization, ' +
                   'accept, origin');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === "OPTIONS") {
         res.send(200);
     } else {
-        next();	
+        next();
     }
 });
 
@@ -38,17 +38,17 @@ ioApp.on('connection', function (socket) {
     var _sessionNumber,
         _bpm,
         _keys,
-        
+
         _notesToPlay,
         _notesPlayed;
-    
+
     socket.on('error', function(exception) {
         console.log('SOCKET ERROR' + exception.toString());
     });
 
-    /* 
+    /*
         Browser web app is initializing a session
-        Must receive this from the front-end: 
+        Must receive this from the front-end:
         {
             bpm -> Beats Per Minute (Integer),
             keys -> # of keys, value ranging from 1 to 128 (Integer)
@@ -56,27 +56,27 @@ ioApp.on('connection', function (socket) {
     */
     socket.on('initializeSession', function (data, callbackFunction) {
         var bpm = data.bpm,
-            keys = data.keys, 
-            
+            keys = data.keys,
+
             sessionNumber = generateSessionNumber();
-        
+
         if (isNaN(Number(bpm)) || bpm < 1) {
-            callbackFunction(new Error("BPM must be a positive, " + 
+            callbackFunction(new Error("BPM must be a positive, " +
                                        "non-zero number."));
             logger.log("BPM must be a positive, non-zero number.");
             return;
         }
         if (isNaN(Number(keys)) || keys < 1 || keys > 128) {
-            callbackFunction(new Error("Keys must be a positive, " + 
-                                       "non-zero number between 1 " + 
+            callbackFunction(new Error("Keys must be a positive, " +
+                                       "non-zero number between 1 " +
                                        "and 128."));
-            logger.log("Keys must be a positive, non-zero number " + 
+            logger.log("Keys must be a positive, non-zero number " +
                        "between 1 and 128.");
             return;
         }
-        
+
         socket.join(sessionNumber);
-        
+
         _sessionNumber = sessionNumber;
         _bpm = bpm;
         _keys = keys;
@@ -84,36 +84,36 @@ ioApp.on('connection', function (socket) {
             sessionNumber: _sessionNumber
         });
     });
-    
+
     /*
         Browser web app is requesting a stream of notes
         Must receive this from the front-end:
         {
-            duration -> # of minutes that denote the length 
+            duration -> # of minutes that denote the length
                 of music to provide as practice for the user. (Integer)
         }
     */
     socket.on('requestNotesStream', function (data, callbackFunction) {
         // Interval of Music Stream (in minutes)
         var duration = data.duration;
-        
+
         // Make sure that parameters of request are valid
         if (_sessionNumber === undefined || _sessionNumber === null) {
             callbackFunction(new Error("Session not initialized. Error."));
             logger.log("Session not initialized. Error.");
             return;
         }
-        if (isNaN(Number(duration)) || 
+        if (isNaN(Number(duration)) ||
             duration < 1) {
-            callbackFunction(new Error("Positive, non-zero music " + 
-                                       "stream duration (in minutes) " + 
+            callbackFunction(new Error("Positive, non-zero music " +
+                                       "stream duration (in minutes) " +
                                        "required."));
-            logger.log("Positive, non-zero music stream duration " + 
+            logger.log("Positive, non-zero music stream duration " +
                        "(in minutes) required.");
             return;
         }
-        
-        // Low and High limit for pitches of notes 
+
+        // Low and High limit for pitches of notes
         var pitchRange = (function () {
                 var leftoverOffset = (128 - _keys) / 2;
                 return {
@@ -121,23 +121,23 @@ ioApp.on('connection', function (socket) {
                     high: Math.floor(_keys + leftoverOffset)
                 };
             }()),
-            
+
             notesStream = generateNotesStream({
-                pitch: pitchRange, 
-                bpm: _bpm, 
+                pitch: pitchRange,
+                bpm: _bpm,
                 duration: duration
             });
-        
+
         _notesToPlay = notesStream;
-        
+
         callbackFunction(notesStream);
-        
+
         socket
             .broadcast
             .to(_sessionNumber)
             .emit('responseNotesStream', notesStream);
     });
-    
+
     /*
         Browser web app is requesting to provide a stream of notes
         Must receive this from the front-end:
@@ -149,27 +149,27 @@ ioApp.on('connection', function (socket) {
     */
     socket.on('provideNotesStream', function (data, callbackFunction) {
         if (_notesToPlay === undefined || _notesToPlay === null) {
-            callbackFunction(new Error("requestNotesStream hasn't been " + 
+            callbackFunction(new Error("requestNotesStream hasn't been " +
                                        "called. Error."));
             logger.log("requestNotesStream hasn't been called. Error");
             return;
         }
-        
+
         // Make sure that parameters of request are valid
         if ([].constructor === Array) {
-            callbackFunction(new Error("You must provide an Array " + 
+            callbackFunction(new Error("You must provide an Array " +
                                        "of Objects"));
             logger.log("You must provide an Array of Objects");
             return;
         }
-        var listFormattedCorrectly = 
+        var listFormattedCorrectly =
             data.reduce(function (previousValue, currentValue) {
                 if (previousValue === false) {
                     return false;
                 }
 
-                if (data.hasOwnProperty('start') && 
-                    data.hasOwnProperty('pitch') && 
+                if (data.hasOwnProperty('start') &&
+                    data.hasOwnProperty('pitch') &&
                     data.hasOwnProperty('duration')) {
                     return true;
                 }
@@ -177,56 +177,56 @@ ioApp.on('connection', function (socket) {
         if (!listFormattedCorrectly) {
             callbackFunction(new Error("You must provide a correctly " +
                                        "formatted Array of Objects"));
-            logger.log("You must provide a correctly formatted " + 
+            logger.log("You must provide a correctly formatted " +
                        "Array of Objects");
             return;
         }
-        
+
         _notesPlayed.concat(data);
-        
+
         var analysis = compare2NotesLists(_notesToPlay, data, _bpm);
-        
+
         callbackFunction(analysis);
         logger.log(JSON.stringify(analysis));
     });
-    
-    
-    /* 
-        Browser web app is requesting its session to be merged to 
+
+
+    /*
+        Browser web app is requesting its session to be merged to
         another session.
     */
     socket.on('joinSession', function (data, callbackFunction) {
         var sessionNumber = data.sessionNumber;
-        
+
         // Make sure that parameters of request are valid
-        if (sessionNumber === undefined || sessionNumber === null || 
+        if (sessionNumber === undefined || sessionNumber === null ||
             String(sessionNumber).length !== 6) {
             callbackFunction(new Error("Session ID is invalid."));
             logger.log(new Error("Session ID is invalid."));
             return;
         }
-        
+
         socket.join(sessionNumber);
         _sessionNumber = sessionNumber;
-        
+
         callbackFunction(sessionNumber);
     });
-    
+
     /*
         Browser web app is requesting to leave a session
     */
     socket.on('leaveSession', function (data, callbackFunction) {
-        
+
         // Make sure that the state of the app is correct
         if (_sessionNumber === undefined || _sessionNumber === null) {
             callbackFunction(new Error("You are not in a session."));
             logger.log(new Error("You are not in a session."));
             return;
         }
-        
+
         socket.leave(_sessionNumber);
         _sessionNumber = generateSessionNumber();
-        
+
         callbackFunction(_sessionNumber);
     });
 
